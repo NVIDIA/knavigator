6eefa64cb1286659f8b2385b6e18d10e9e1e18b5
Shinae Woo <shinaew@nvidia.com>
2024-05-21 13:46:18 -0700

    Cleanup prev Go GitAction
    Signed-off-by: Shinae Woo <shinaew@nvidia.com>

--- tests/ci/test_k8s.sh
+++ tests/ci/test_k8s.sh
@@ -6,8 +6,8 @@ REPO_HOME=$(readlink -f $(dirname $(readlink -f "$0"))/../../)
 KWOK_REPO=kubernetes-sigs/kwok
 KWOK_LATEST_RELEASE=v0.5.2
 
-kubectl apply -f "https://github.com/${KWOK_REPO}/releases/download/${KWOK_LATEST_RELEASE}/kwok.yaml"
-kubectl apply -f "https://github.com/${KWOK_REPO}/releases/download/${KWOK_LATEST_RELEASE}/stage-fast.yaml"
+kubectl apply -f https://github.com/${KWOK_REPO}/releases/download/${KWOK_LATEST_RELEASE}/kwok.yaml
+kubectl apply -f https://github.com/${KWOK_REPO}/releases/download/${KWOK_LATEST_RELEASE}/stage-fast.yaml
 kubectl apply -f https://github.com/${KWOK_REPO}/raw/main/kustomize/stage/pod/chaos/pod-init-container-running-failed.yaml
 kubectl apply -f https://github.com/${KWOK_REPO}/raw/main/kustomize/stage/pod/chaos/pod-container-running-failed.yaml
 kubectl apply -f ${REPO_HOME}/charts/overrides/kwok/pod-complete.yml
