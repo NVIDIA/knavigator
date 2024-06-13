c1357267a30af1ad23d601798279c8166003d646
Dmitry Shmulevich <dshmulevich@nvidia.com>
2024-06-13 09:27:56 -0700

    update CI tests
    Signed-off-by: Dmitry Shmulevich <dshmulevich@nvidia.com>

--- tests/ci/test_volcano.sh
+++ tests/ci/test_volcano.sh
@@ -1,24 +1,18 @@
 #! /bin/bash -x
 
-REPO_HOME=$(readlink -f $(dirname $(readlink -f "$0"))/../../)
+export REPO_HOME=$(readlink -f $(dirname $(readlink -f "$0"))/../../)
 
 # Install KWOK node simulator
-KWOK_REPO=kubernetes-sigs/kwok
-KWOK_LATEST_RELEASE=v0.5.2
-
-kubectl apply -f https://github.com/${KWOK_REPO}/releases/download/${KWOK_LATEST_RELEASE}/kwok.yaml
-kubectl apply -f https://github.com/${KWOK_REPO}/releases/download/${KWOK_LATEST_RELEASE}/stage-fast.yaml
-kubectl apply -f https://github.com/${KWOK_REPO}/raw/main/kustomize/stage/pod/chaos/pod-init-container-running-failed.yaml
-kubectl apply -f https://github.com/${KWOK_REPO}/raw/main/kustomize/stage/pod/chaos/pod-container-running-failed.yaml
-kubectl apply -f ${REPO_HOME}/charts/overrides/kwok/pod-complete.yml
+${REPO_HOME}/scripts/install-kwok.sh
 
 # Install Volcano
 helm repo add volcano-sh https://volcano-sh.github.io/helm-charts
+helm repo update
 helm install volcano volcano-sh/volcano -n volcano-system --create-namespace --wait
 
-# Wait until volcano webhook is ready
-# TODO: we need a deterministric way to check if it's ready
-sleep 10
+for app in volcano-admission volcano-controller volcano-scheduler; do
+  kubectl -n volcano-system wait --for=condition=ready pod -l app=$app --timeout=60s
+done
 
 # Run knavigator with an example test
 ${REPO_HOME}/bin/knavigator -workflow ${REPO_HOME}/resources/workflows/volcano/test-job.yml
