856b95949921d1fd8a9d0623a682d75cacf37a9f
Dmitry Shmulevich <dshmulevich@nvidia.com>
2024-07-28 12:03:54 -0700

    update scripts (#86)
    Signed-off-by: Dmitry Shmulevich <dshmulevich@nvidia.com>

--- scripts/env.sh
+++ scripts/env.sh
@@ -5,15 +5,27 @@ YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
+function printRed() {
+  echo -e "${RED}$@${NC}"
+}
+
+function printGreen() {
+  echo -e "${GREEN}$@${NC}"
+}
+
 function printYellow() {
   echo -e "${YELLOW}$@${NC}"
+}
+
+function printBlue() {
+  echo -e "${BLUE}$@${NC}"
 }
 
 ### check for command
 function fail_if_command_not_found() {
   local command_name="$1"
   if ! command -v $command_name &> /dev/null; then
-    echo "$command_name is not installed"
+    printRed "$command_name is not installed"
     exit 1
   fi
 }
@@ -25,6 +37,8 @@ KWOK_REPO=kubernetes-sigs/kwok
 KWOK_RELEASE="v0.6.0"
 
 function deploy_kwok() {
+  printGreen Deploying KWOK
+
   # Deploy KWOK controller
   kubectl apply -f https://github.com/${KWOK_REPO}/releases/download/${KWOK_RELEASE}/kwok.yaml
 
@@ -38,18 +52,24 @@ function deploy_kwok() {
 # Prometheus
 #
 
-function deploy_prometheus() {
-  helm repo add --force-update prometheus-community \
-  https://prometheus-community.github.io/helm-charts
+PROMETHEUS_STACK_VERSION=61.5.0
 
-  helm install -n monitoring --create-namespace kube-prometheus-stack \
+function deploy_prometheus() {
+  printGreen Deploying Prometheus
+
+  helm repo add --force-update prometheus-community https://prometheus-community.github.io/helm-charts
+
+  helm upgrade --install -n monitoring --create-namespace kube-prometheus-stack \
     prometheus-community/kube-prometheus-stack \
+    --version=$PROMETHEUS_STACK_VERSION --wait \
     --set alertmanager.enabled=false \
     --set defaultRules.rules.alertmanager=false \
     --set defaultRules.rules.nodeExporterAlerting=false \
     --set defaultRules.rules.nodeExporterRecording=false \
     --set prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues=false \
     --set prometheus.prometheusSpec.podMonitorSelectorNilUsesHelmValues=false
+
+  kubectl -n monitoring wait --for=condition=ready pod -l app.kubernetes.io/instance=kube-prometheus-stack --timeout=3m
 }
 
 # Tested workload managers
@@ -62,21 +82,29 @@ KUEUE_VERSION=v0.8.0
 # https://github.com/volcano-sh/volcano
 VOLCANO_VERSION=v1.9.0
 # https://github.com/apache/yunikorn-core
-YUNIKORN_VERSION=v1.5.2
+YUNIKORN_VERSION=v1.5.1
 
 function deploy_jobset() {
+  printGreen Deploying jobset
+
   kubectl apply --server-side -f https://github.com/kubernetes-sigs/jobset/releases/download/${JOBSET_VERSION}/manifests.yaml
   kubectl -n jobset-system wait --for=condition=ready pod -l control-plane=controller-manager --timeout=60s
 }
 
 function deploy_kueue() {
+  printGreen Deploying kueue
+
   kubectl apply --server-side -f https://github.com/kubernetes-sigs/kueue/releases/download/${KUEUE_VERSION}/manifests.yaml
   kubectl -n kueue-system wait --for=condition=ready pod -l control-plane=controller-manager --timeout=60s
 }
 
 function deploy_volcano() {
+  printGreen Deploying volcano
+
   helm repo add --force-update volcano-sh https://volcano-sh.github.io/helm-charts
-  helm install volcano volcano-sh/volcano -n volcano-system --create-namespace --wait
+
+  helm upgrade --install volcano volcano-sh/volcano -n volcano-system --create-namespace \
+    --version=$VOLCANO_VERSION --wait
 
   for app in volcano-admission volcano-controller volcano-scheduler; do
     kubectl -n volcano-system wait --for=condition=ready pod -l app=$app --timeout=60s
@@ -88,8 +116,12 @@ function deploy_volcano() {
 }
 
 function deploy_yunikorn() {
+  printGreen Deploying yunikorn
+
   helm repo add --force-update yunikorn https://apache.github.io/yunikorn-release
-  helm install yunikorn yunikorn/yunikorn -n yunikorn --create-namespace --wait
+
+  helm upgrade --install yunikorn yunikorn/yunikorn -n yunikorn --create-namespace \
+    --version=$YUNIKORN_VERSION --wait
 
   kubectl -n yunikorn wait --for=condition=ready pod -l app=yunikorn --timeout=60s
 }
