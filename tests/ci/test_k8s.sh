04bf1f7af673729bce719a7cc656ff30947c9fe7
Dmitry Shmulevich <dshmulevich@nvidia.com>
2024-08-02 06:40:47 -0700

    upgrade yunikorn to v1.5.2
    Signed-off-by: Dmitry Shmulevich <dshmulevich@nvidia.com>

--- tests/ci/test_k8s.sh
+++ tests/ci/test_k8s.sh
@@ -9,4 +9,4 @@ source $REPO_HOME/scripts/env.sh
 deploy_kwok
 
 # Run knavigator with an example test
-${REPO_HOME}/bin/knavigator -workflow ${REPO_HOME}/resources/workflows/k8s/test-job.yml
+${REPO_HOME}/bin/knavigator -workflow ${REPO_HOME}/resources/workflows/k8s/test-job.yml -v 4 -cleanup
